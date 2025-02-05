3a45fc95a952bb0e3c1aae75a60e77113cc2ebc4
MaximZamotaev <112868632+MaximZamotaev@users.noreply.github.com>
2025-02-05 12:09:03 +0300

    1.4 Create Clients_with_validation.py

--- Lab_1/Clients_with_validation.py
+++ Lab_1/Clients_with_validation.py
@@ -1,4 +1,4 @@
-class Client:
+class Clientы:
     def __init__(self, client_id, fullname, phone_number, email):
         # Валидируем поля при создании объекта
         if not self.validate_client_id(client_id):
@@ -10,44 +10,44 @@ class Client:
         if not self.validate_email(email):
             raise ValueError("Некорректный email. Проверьте формат: должен быть в виде 'что-то@что-то.домен'.")
 
-        self._client_id = client_id
-        self._fullname = fullname
-        self._phone_number = phone_number
-        self._email = email
+        self.client_id = client_id
+        self.fullname = fullname
+        self.phone_number = phone_number
+        self.email = email
 
     # Геттеры и сеттеры с валидацией
 
     def get_client_id(self):
-        return self._client_id
+        return self.client_id
 
     def set_client_id(self, client_id):
         if not self.validate_client_id(client_id):
             raise ValueError("Некорректный идентификатор клиента. Идентификатор должен быть положительным целым числом.")
-        self._client_id = client_id
+        self.client_id = client_id
 
     def get_fullname(self):
-        return self._fullname
+        return self.fullname
 
     def set_fullname(self, fullname):
         if not self.validate_fullname(fullname):
             raise ValueError("Некорректное полное имя. Полное имя не может быть пустым.")
-        self._fullname = fullname
+        self.fullname = fullname
 
     def get_phone_number(self):
-        return self._phone_number
+        return self.phone_number
 
     def set_phone_number(self, phone_number):
         if not self.validate_phone_number(phone_number):
             raise ValueError("Некорректный номер телефона. Должен быть строкой из цифр и, возможно, начинаться с '+'.")
-        self._phone_number = phone_number
+        self.phone_number = phone_number
 
     def get_email(self):
-        return self._email
+        return self.email
 
     def set_email(self, email):
         if not self.validate_email(email):
             raise ValueError("Некорректный email. Проверьте формат: должен быть в виде 'что-то@что-то.домен'.")
-        self._email = email
+        self.email = email
 
     # Статические методы для валидации
     @staticmethod
@@ -71,4 +71,4 @@ class Client:
         return isinstance(email, str) and re.fullmatch(r'[^@]+@[^@]+\.[^@]+', email) is not None
 
     def __str__(self):
-        return f"Client{{client_id={self._client_id}, fullname='{self._fullname}', phone_number='{self._phone_number}', email='{self._email}'}}"
+        return f"Client{{client_id={self.client_id}, fullname='{self.fullname}', phone_number='{self.phone_number}', email='{self.email}'}}"
