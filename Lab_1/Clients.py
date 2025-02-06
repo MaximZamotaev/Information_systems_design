8b1b21526c0cf6ccf6c3448c391c50a189cefc2d
MaximZamotaev <112868632+MaximZamotaev@users.noreply.github.com>
2025-02-06 20:43:42 +0300

    1.3 Create Clients.py

--- Lab_1/Clients.py
+++ Lab_1/Clients.py
@@ -1,34 +1,34 @@
 class Clients:
     def __init__(self, client_id, fullname, phone_number, email):
-        self.client_id = client_id
-        self.fullname = fullname
-        self.phone_number = phone_number
-        self.email = email
+        self._client_id = client_id
+        self._fullname = fullname
+        self._phone_number = phone_number
+        self._email = email
 
     def get_client_id(self):
-        return self.client_id
+        return self._client_id
 
     def set_client_id(self, client_id):
-        self.client_id = client_id
+        self._client_id = client_id
 
     def get_fullname(self):
-        return self.fullname
+        return self._fullname
 
     def set_fullname(self, fullname):
-        self.fullname = fullname
+        self._fullname = fullname
 
     def get_phone_number(self):
-        return self.phone_number
+        return self._phone_number
 
     def set_phone_number(self, phone_number):
-        self.phone_number = phone_number
+        self._phone_number = phone_number
 
     def get_email(self):
-        return self.email
+        return self._email
 
     def set_email(self, email):
-        self.email = email
+        self._email = email
 
     def __str__(self):
-        return f"Client{{client_id={self.client_id}, fullname='{self.fullname}', phone_number='{self.phone_number}', email='{self.email}'}}"
+        return f"Client{{client_id={self._client_id}, fullname='{self._fullname}', phone_number='{self._phone_number}', email='{self._email}'}}"
       
